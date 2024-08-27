7836a6b3a8a8bfaaf274c3d305d6fb356b386f62
ChHsiching <hsichingchang@gmail.com>
2024-08-27 09:56:22 +0800

    Write a demo to test the CRUD functionality of mybatis-plus

---
+++ src/main/java/com/chhsich/usercenterbackend/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.chhsich.usercenterbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chhsich.usercenterbackend.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
