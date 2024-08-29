5abe132eae5badc05acf5ea137e694bf13af3dc8
ChHsiching <hsichingchang@gmail.com>
2024-08-29 20:42:53 +0800

    Organize project directory

--- src/main/java/com/chhsich/usercenterbackend/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.chhsich.usercenterbackend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.chhsich.usercenterbackend.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
